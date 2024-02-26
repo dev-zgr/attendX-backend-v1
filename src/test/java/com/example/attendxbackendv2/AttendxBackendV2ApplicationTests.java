d1b931cc47f4fc194f338b76a4f498dd0d0d79e9
= <=>
2024-02-26 17:45:15 +0300

    Initial commit

---
+++ src/test/java/com/example/attendxbackendv2/AttendxBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendxbackendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendxBackendV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
